77d3a640357a50fa6f4c5d6adfea6a25b12edd9c
Sujith Sudhakaran <sudhakaran.sujith1@gmail.com>
2016-06-22 15:51:01 +0530

    Updating the bulk insert response message.

--- routes/bulkInsert.js
+++ routes/bulkInsert.js
@@ -1,15 +1,17 @@
 module.exports = function(app) {
 
   app.get('/insert', function(req, res) {
-    for (var i = 0; i < 100000; i++) {
+    var totalRecords = 100000;
+    for (var i = 0; i < totalRecords; i++) {
       app.get('dbConn').model('Bulk').create({id: i});
     }
-    res.send({success: true, msg: 'Inserted 100000 records in bulk collections.'});
+    res.send({success: true, msg: 'Inserted records count: ' + totalRecords});
   });
 
   app.get('/bulk', function(req, res) {
     var bulk = app.get('dbConn').model('Bulk').collection.initializeUnorderedBulkOp();
-    for (var i = 0; i < 100000; i++) {
+    var totalRecords = 1000000;
+    for (var i = 0; i < totalRecords; i++) {
       bulk.insert({id:i});
     }
     bulk.execute(function(err, savedRecords) {
@@ -18,7 +20,7 @@ module.exports = function(app) {
         res.send({success: false, msg: 'Failure in bulk save.'});
       } else {
         console.log('##### savedRecords: ', savedRecords.nInserted);
-        res.send({success: true, msg: 'Inserted 100000 records in bulk collections.'});
+        res.send({success: true, msg: 'Inserted records count: ' + totalRecords});
       }
     });
   });
